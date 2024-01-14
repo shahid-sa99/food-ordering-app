b398990e590d3a37e1e21e6927a7640b419b4f62
shahid-sa99 <sheikhmannanshahid1999@gmail.com>
2024-01-14 14:22:05 +0530

    changes

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 // const heading = React.createElement("h1",{id:"heading",set:"tuio"},"Hello world from React");/
 
 const parent = React.createElement("div",
-{id:"parent"},
+{id:"parent1"},
 [React.createElement("div",{id:"child1"},[React.createElement("h1",{},"Im h1 tag"),React.createElement("h2",{},"Im h2 tag")]),
 React.createElement("div",{id:"child2"},[React.createElement("h1",{},"Im child2 h1 tag"),React.createElement("h2",{},"Im child2 h2 tag")])
 ])
