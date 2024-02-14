172e3288008af2427516fff89aec92686cef7445
shahid-sa99 <sheikhmannanshahid1999@gmail.com>
2024-02-14 19:40:42 +0530

    functional component

--- App.js
+++ App.js
@@ -3,36 +3,11 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement("div",
-{id:"paret"},
-[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"Im react tag"),React.createElement("h2",{},"Im h2 tag")]),
-React.createElement("div",{id:"child2"},[React.createElement("h1",{},"Im child2 node teghfghfh fhfshgd g"),React.createElement("h2",{},"Im child2 h2 tag")])
-])
-
-
-// console.log(heading)
-
-const element = <h2>React element</h2>
-
-const HeadingOldComponent = function(){
-    return(
-        <h3>React Old</h3>
-    )
+const HeadingComponent=()=>{
+    return(<h1>test component</h1>)
 }
 
-
-const HeadingComponent = ()=>(
-    <>
-    <HeadingOldComponent/>
-    
-    {element}
-    <h1>Test</h1>
-    </>
-)
-
-//creating react root that will render h1 in the root element 
-const root = ReactDOM.createRoot(document.getElementById("root1"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-//the react Object is converted into actual HTML using the render function
 root.render(<HeadingComponent/>);
