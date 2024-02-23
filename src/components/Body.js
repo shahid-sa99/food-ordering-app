e59c8614b390e85e6a25033d6e5e7d5310361010
shahid-sa99 <sheikhmannanshahid1999@gmail.com>
2024-02-23 12:55:14 +0530

    added search func in dashboard

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,35 +3,53 @@ import { resList } from "../common/mockData";
 import RestuarantCard from "./RestuarantCard";
 const Body = () => {
   const [restuarantList, setRestuarantList] = useState(resList);
+  const [filteredRestuarantList,setFilteredRestuarantList] = useState(resList);
+  const [searchText, setSearchext] = useState("");
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  useEffect(() => {}, []);
 
   const fetchData = async () => {
     // const data = await fetch(
     //   "https://cors-anywhere.herokuapp.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     // );
-    // console.log(data);
+    
   };
 
-  console.log("body rendered");
   return (
     <div className="body">
-      {/* <div className="search">Search</div> */}
-      <button
-        className="filter-res-btn"
-        onClick={() => {
-          const filteredList = restuarantList.filter(
-            (res) => res.data.rating > 4.5
-          );
-          setRestuarantList(filteredList);
-        }}
-      >
-        Filter restuarants
-      </button>
+      <div className="filter-ctnr">
+        <div className="search">
+          <input
+            value={searchText}
+            onChange={(e) => {
+              setSearchext(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredList = restuarantList.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestuarantList(filteredList)
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-res-btn"
+          onClick={() => {
+            const filteredList = filteredRestuarantList.filter(
+              (res) => res.info.avgRating >= 4.5
+            );
+            setFilteredRestuarantList(filteredList);
+          }}
+        >
+          Filter restuarants
+        </button>
+      </div>
       <div className="res-container">
-        {restuarantList.map((restuarant) => (
+        {filteredRestuarantList.map((restuarant) => (
           <RestuarantCard key={restuarant.info.id} resData={restuarant} />
         ))}
       </div>
