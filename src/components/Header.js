dd94fbe1f8bea214cbd460662abab73b91934a96
shahid-sa99 <sheikhmannanshahid1999@gmail.com>
2024-02-23 16:58:59 +0530

    changes in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
-const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"
-          ></img>
-        </div>
-        <div className="nav-bar">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+import { useState } from "react";
 
-  export default Header;
+const Header = () => {
+  const [btnText, setBtnText] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"
+        ></img>
+      </div>
+      <div className="nav-bar">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() =>
+              btnText === "Login" ? setBtnText("Logout") : setBtnText("Login")
+            }
+          >
+            {btnText}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
