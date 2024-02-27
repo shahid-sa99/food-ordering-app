cc9f9bd3ce297a66a2b8684e9fd48880abe13eb2
shahid-sa99 <sheikhmannanshahid1999@gmail.com>
2024-02-28 02:37:57 +0530

    class based components-1

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,35 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About us page</h1>
-    </div>
-  );
-};
+import { Component, useState } from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
+
+  componentDidMount() {
+    console.log("parent componentDidMount");
+  }
+
+  render() {
+    console.log("parent render");
+    return (
+      <div>
+        <h1>About us page</h1>
+        <UserClass name={"Shahid222"} location={"Bengaluru111"} />
+      </div>
+    );
+  }
+}
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About us page</h1>
+//       <UserClass name={"Shahid222"} location={"Bengaluru111"} />
+//     </div>
+//   );
+// };
 
 export default About;
